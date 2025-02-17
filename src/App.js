d201da7dc913cf92d8d018df8fe9b97f571cbba9
kasianiemiec <kasia.a.niemiec@gmail.com>
2025-02-17 23:03:39 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <p>
-        <a href="https://github.com/kasianiemiec/react-app-yes">
-          Open-source code
-        </a>{" "}
-        by Kasia Niemiec.
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <p>
+          <a href="https://github.com/kasianiemiec/react-app-yes">
+            Open-source code
+          </a>{" "}
+          by Kasia Niemiec.
+        </p>
+      </div>
     </div>
   );
 }
